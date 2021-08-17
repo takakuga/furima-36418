58f2897d4dd0311dfb2522fb628a202c369c0ab6
takakuga <sazanami398@gmail.com>
2021-08-18 00:43:49 +0900

    単体テストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
+         validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+         validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+         validates :last_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
          validates :birthday, presence: true
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    first_name            {'真島'}
+    last_name             {'吾郎'}
+    first_name_kana       {'マジマ'}
+    last_name_kana        {'ゴロウ'}
+    birthday              { Faker::Date.between(from: '1989-01-01', to: '2016-12-31') }
+  end
+end
