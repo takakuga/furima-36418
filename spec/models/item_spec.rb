06bd7160bfd2abd04632a901d6144df3bef49db8
takakuga <sazanami398@gmail.com>
2021-08-20 15:30:00 +0900

    2度目の修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -77,6 +77,11 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include('Price Out of setting range')
     end
+    it 'ユーザーが紐付いていないと出品できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('User must exist')
+    end
    end
   end
 end
